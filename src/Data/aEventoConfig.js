e57366b66f7ecf39563bf8aa61b0bf4bb1d48de9
onitseDv <onitsedv@hotmail.com>
2022-04-05 13:58:22 -0300

    commit

--- src/Data/aEventoConfig.js
+++ src/Data/aEventoConfig.js
@@ -1,18 +1,36 @@
 import { Kafka } from 'kafkajs';
 
-const topic = 'fixture-events';
-const kafka = new Kafka({ brokers: ['localhost:9092'] });
+const kafka = new Kafka({
+    brokers: ['localhost:9092'],
+    clientId: 'certificate',
+})
+
+const topic = 'issue-certificate'
+const consumer = kafka.consumer({ groupId: 'certificate-group' })
+
 const producer = kafka.producer();
 
-const enviaEvento = async (message) => {
-  console.log(`Mensagem: ${message}`);
-  await producer.connect();
-  await producer.send({
-    topic,
-    messages: [
-      { value: message },
-    ],
-  });
-};
+async function run() {
+    await consumer.connect()
+    await consumer.subscribe({ topic })
 
-module.exports = {enviaEvento};
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+            console.log(`- ${prefix} ${message.key}#${message.value}`)
+
+      const payload = JSON.parse(message.value);
+
+      // setTimeout(() => {
+      producer.send({
+        topic: 'certification-response',
+        messages: [
+          { value: `Certificado do usuário ${payload.user.name} do curso ${payload.course} gerado!` }
+        ]
+      })
+      // }, 3000);
+    },
+  })
+}
+
+run().catch(console.error)
